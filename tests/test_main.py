19e4e0b907cd98874d2d153f536286745a233849
FransvanEk <frans@fransvanek.nl>
2024-11-18 12:58:14 +0100

    fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,4 +16,4 @@ client = TestClient(app)
 def test_greet():
     response = client.get("/greet/Frans")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hallo, Frans!"}
+    assert response.json() == {"message": "Hallo, Frans!!!!!"}
